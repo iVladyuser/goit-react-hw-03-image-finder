be3c2b4e4e09bf6e814fe4e662320c7a4fd70d7d
iVladyuser <korol.v09@gmail.com>
2023-10-24 18:31:12 +0200

    add components

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Gallery } from './ImageGallery.styles';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+
+const ImageGallery = () => {
+  return <Gallery>
+    <ImageGalleryItem/>
+  </Gallery>;
+};
+
+export default ImageGallery;
