f7b599834680a4b6c038fcfecdf31b0276c3c59b
iVladyuser <korol.v09@gmail.com>
2023-10-26 19:43:06 +0200

    add-fetch-data

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import { Gallery } from './ImageGallery.styles';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 
-const ImageGallery = () => {
-  return <Gallery>
-    <ImageGalleryItem/>
-  </Gallery>;
-};
+const ImageGallery = ({photos}) => (
+  <Gallery>
+    {photos !== null && photos.map(({ webformatURL, id, tags }) => (
+      <ImageGalleryItem data-id={id} tags={tags} smallUrl={webformatURL} />
+    ))}
+  </Gallery>
+);
 
 export default ImageGallery;
