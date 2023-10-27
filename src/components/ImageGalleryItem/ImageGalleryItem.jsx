91e4028ce8549aefe72eecc2ac90342ddd7c1649
iVladyuser <korol.v09@gmail.com>
2023-10-27 12:01:39 +0200

    finish with modal

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { GalleryItem, GalleryImage } from './ImageGalleryItem.styles';
 
-const ImageGalleryItem = ({ id, tags, smallUrl, onClickImageItem }) => {
-  return (
-    <GalleryItem key={id} data-id={id} onClick={onClickImageItem}>
+
+const ImageGalleryItem = ({id, smallUrl, tags, onClickImageItem }) => (
+  <GalleryItem
+      key={id}
+      data-id={id}
+      onClick={onClickImageItem}
+  >
       <GalleryImage src={smallUrl} alt={tags} data-id={id} />
-    </GalleryItem>
-  );
-};
+  </GalleryItem>
+);
+
 
 export default ImageGalleryItem;
